a7b9549e80d43824473fdca5d57ace5a2b1c5a6d
Aleksandr <alokil70@gmail.com>
2023-02-04 17:13:15 +0300

    fragments added

---
+++ app/src/main/java/com/example/gamescore/presentation/GameFinishedFragment.kt
@@ -0,0 +1,25 @@
+package com.example.gamescore.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gamescore.R
+import com.example.gamescore.databinding.FragmentChooseLevelBinding
+import com.example.gamescore.databinding.FragmentGameFinishedBinding
+
+class GameFinishedFragment : Fragment() {
+
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
